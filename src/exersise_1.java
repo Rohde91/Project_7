550cf2e0a4630ae523b67c05da34feeca9e446d5
Martin Rohde <mart3b24@edu.easj.dk>
2021-09-21 09:05:24 +0200

    exersise_1 done

---
+++ src/exersise_1.java
@@ -0,0 +1,33 @@
+public class exersise_1 {
+    public static int banana(int[] a) {
+        int kiwi = 1;
+        int i = 0;
+        while (i < a.length) {
+            kiwi = kiwi * a[i];
+            i++;
+        }
+        return kiwi;
+    }
+    //returns kiwi or kiwi * i++
+
+    public static int grapefruit(int[] a, int grape) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == grape) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    //returns -1 or the value of i
+
+    public static int pineapple(int[] a, int apple) {
+        int pear = 0;
+        for (int pine: a) {
+            if (pine == apple) {
+                pear++;
+            }
+        }
+        return pear;
+    }
+    //returns pear or pear++
+}
